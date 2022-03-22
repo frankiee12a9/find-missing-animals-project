ae6b8fadd6d6cd659a7b7ed260bb7205d5280aad
Kien Nguyen The <cudayanh@gmail.com>
2022-03-22 11:45:59 +0900

    mapping domain to Dtos

--- Modules/Core/MappingProfiles.cs
+++ Modules/Core/MappingProfiles.cs
@@ -1,10 +1,18 @@
 using System;
-// using Application.Activities;
-// using Application.Comments;
-// using Application.Interfaces;
+using System.Linq;
+using System.Runtime.ExceptionServices;
 // using Application.Profiles;
 using AutoMapper;
 using Domain;
+using Modules.AppUsers;
+using Modules.Categories;
+using Modules.Comments;
+// using Application.Activities;
+// using Application.Comments;
+using Modules.Interfaces;
+using Modules.Locations;
+using Modules.Posts;
+using Modules.Tags;
 
 namespace Modules.Core
 {
@@ -12,7 +20,51 @@ namespace Modules.Core
 	{
 		public MappingProfiles()
 		{
-			Console.WriteLine("object mapping");
+			string currentUsername = "";
+
+			CreateMap<Post, Post>();
+
+			CreateMap<Tag, TagDto>()
+				.ForMember(dest => dest.Id, o => o.MapFrom(src => src.Id))
+				.ForMember(dest => dest.TagName, o => o.MapFrom(src => src.TagName));
+
+			CreateMap<PostTag, TagDto>()
+				.ForMember(dest => dest.Id, o => o.MapFrom(src => src.TagId))
+				.ForMember(dest => dest.TagName, o => o.MapFrom(src => src.Tag.TagName));
+
+			CreateMap<Category, CategoryDto>()
+				.ForMember(dest => dest.Id, o => o.MapFrom(src => src.Id))
+				.ForMember(dest => dest.CategoryName, o => o.MapFrom(src => src.CategoryName));
+
+			// mapping PostFollowing - PostParticipant
+			// CreateMap<PostFollowing, PostParticipantDto>()
+			// 	.ForMember(dest => dest.DisplayName, o => o.MapFrom(src => src.ApplicationUser.DisplayName));
+
+			CreateMap<PostLocation, PostLocationDto>()
+				.ForMember(dest => dest.Id, o => o.MapFrom(src => src.Id))
+				.ForMember(dest => dest.PostCode, o => o.MapFrom(src => src.PostCode))
+				.ForMember(dest => dest.RoadLocation, o => o.MapFrom(src => src.RoadLocation))
+				.ForMember(dest => dest.Location, o => o.MapFrom(src => src.Location))
+				.ForMember(dest => dest.ExtraLocation, o => o.MapFrom(src => src.ExtraLocation))
+				.ForMember(dest => dest.Location, o => o.MapFrom(src => src.Location))
+				.ForMember(dest => dest.Latitude, o => o.MapFrom(src => src.Latitude));
+
+			CreateMap<Post, PostDto>()
+				.ForMember(dest => dest.PosterName,
+				o => o.MapFrom(src => src.PostFollowers.FirstOrDefault(x => x.isPoster).ApplicationUser.UserName))
+				.ForMember(dest => dest.PostLocation, o => o.MapFrom(src => src.PostLocation))
+				.ForMember(dest => dest.Tags, o => o.MapFrom(src => src.PostTags))
+				.ForMember(dest => dest.Category, o => o.MapFrom(src => src.Category));
+
+			// CreateMap<ApplicationUser, PostParticipantDto>()
+			// 	.ForMember(dest => dest.Image, o => o.MapFrom(x => x.Photos.FirstOrDefault(p => p.IsProfilePicture).Url));
+
+			CreateMap<Comment, CommentDto>()
+				.ForMember(dest => dest.DisplayName, o => o.MapFrom(src => src.ApplicationUser.DisplayName))
+				.ForMember(dest => dest.Username, o => o.MapFrom(src => src.ApplicationUser.UserName))
+				.ForMember(dest => dest.ImageUrl, o => o.MapFrom(src => src.ApplicationUser.Photos
+					.FirstOrDefault(x => x.IsProfilePicture).Url));
+
 		}
 	}
 }
