c7e03556c1c67f2bbe8a575219ccbf84565b9501
Kien Nguyen The <cudayanh@gmail.com>
2022-03-24 07:24:17 +0900

    AppUsers module

---
+++ Modules/AppUsers/AppUserDto.cs
@@ -0,0 +1,10 @@
+namespace Modules.AppUsers
+{
+	public class PostParticipantDto
+	{
+		public string Username { get; set; }
+		public string DisplayName { get; set; }
+		public string Bio { get; set; }
+		public string Image { get; set; }
+	}
+}
