6503b089465bdc55b95e9c9538ee2b3798cfa1f2
Kien Nguyen The <cudayanh@gmail.com>
2022-03-24 07:36:31 +0900

    remove 'get address' from Account

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -97,7 +97,6 @@ const Account = {
 	login: (values: any) => requests.post("account/login", values),
 	register: (values: any) => requests.post("account/register", values),
 	currentUser: () => requests.get("account/currentUser"),
-	// fetchAddress: () => requests.get("account/savedAddress"),
 }
 
 const agent = {
