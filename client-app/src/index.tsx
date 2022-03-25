dd1179647f2fd64d60bdde18f906fff20f5721a4
Kien Nguyen The <cudayanh@gmail.com>
2022-03-25 12:54:09 +0900

    install new packages

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./app/layout/App"
+import reportWebVitals from "./reportWebVitals"
+import { Provider } from "react-redux"
+import { createBrowserHistory, History } from "history"
+import { Router } from "react-router-dom"
+import { store } from "./app/store/storeConfig"
+
+// Note: currently using react-router-dom v6
+// comparison: https://dev.to/arunavamodak/react-router-v5-vs-v6-dp0
+export const history: History = createBrowserHistory()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<Router history={history}>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
