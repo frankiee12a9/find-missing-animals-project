d60e30dd8b7b57de4046f558722d33ab59677213
Kien Nguyen The <cudayanh@gmail.com>
2022-04-03 16:09:12 +0900

    update Dtos for 'AppUsers'

---
+++ Modules/AppUsers/Dtos/PostParticipantDto.cs
@@ -0,0 +1,11 @@
+namespace Modules.AppUsers.Dtos
+{
+	public class PostParticipantDto
+	{
+		public string Username { get; set; }
+		public string DisplayName { get; set; }
+		public string Bio { get; set; }
+		public string Image { get; set; }
+		public bool IsParticipating { get; set; }
+	}
+}
