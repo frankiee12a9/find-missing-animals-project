38e972a320bae6da073dfeb3b73be58048a6dbe7
Kien Nguyen The <cudayanh@gmail.com>
2022-04-03 16:10:07 +0900

    update 'Tag' features

---
+++ Modules/Tags/ListAllTags.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Modules.Core;
+using Modules.Interfaces;
+using Persistence;
+
+namespace Modules.Tags
+{
+	public class ListAllTags
+	{
+		public class Query : IRequest<Result<List<TagDto>>>
+		{
+			// public int TagId { get; set; }
+		}
+
+		public class Handler : IRequestHandler<Query, Result<List<TagDto>>>
+		{
+			private readonly AppDataContext _context;
+			private readonly ILogger<ListAllTags> _logger;
+			private readonly IMapper _mapper;
+			private readonly IUserAccessor _userAccessor;
+
+			public Handler(AppDataContext context, ILogger<ListAllTags> logger, IMapper mapper, IUserAccessor userAccessor)
+			{
+				_context = context;
+				_logger = logger; ;
+				_mapper = mapper;
+				_userAccessor = userAccessor;
+			}
+
+			public async Task<Result<List<TagDto>>> Handle(Query request, CancellationToken cancellationToken)
+			{
+				// var result = await _context.Tags
+				// 	.OrderBy(x => x.TagName)
+				// 	.ProjectTo<TagDto>(_mapper.ConfigurationProvider)
+				// 	.ToListAsync();
+
+				// return Result<List<TagDto>>.Success(result);
+
+				return Result<List<TagDto>>.Success(null);
+			}
+		}
+	}
+}
