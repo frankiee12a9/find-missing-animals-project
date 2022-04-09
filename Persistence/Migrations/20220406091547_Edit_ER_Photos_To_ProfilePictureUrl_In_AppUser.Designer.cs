525aaaedf1932062e87099394376328d4d08ec7f
Kien Nguyen The <cudayanh@gmail.com>
2022-04-09 19:01:03 +0900

    update newly added migrations

--- Persistence/Migrations/20220406091547_Edit_ER_Photos_To_ProfilePictureUrl_In_AppUser.Designer.cs
+++ Persistence/Migrations/20220406091547_Edit_ER_Photos_To_ProfilePictureUrl_In_AppUser.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AppDataContext))]
-    partial class AppDataContextModelSnapshot : ModelSnapshot
+    [Migration("20220406091547_Edit_ER_Photos_To_ProfilePictureUrl_In_AppUser")]
+    partial class Edit_ER_Photos_To_ProfilePictureUrl_In_AppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/20220406091547_Edit_ER_Photos_To_ProfilePictureUrl_In_AppUser.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class Edit_ER_Photos_To_ProfilePictureUrl_In_AppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_AspNetUsers_ApplicationUserId",
+                table: "Photos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Photos_ApplicationUserId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "IsProfilePicture",
+                table: "Photos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureUrl",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Photos",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProfilePicture",
+                table: "Photos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_ApplicationUserId",
+                table: "Photos",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_AspNetUsers_ApplicationUserId",
+                table: "Photos",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
