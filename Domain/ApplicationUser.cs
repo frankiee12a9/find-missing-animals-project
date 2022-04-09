b2910852950591ec6dfc7219eb4ff42152b5783a
Kien Nguyen The <cudayanh@gmail.com>
2022-04-09 18:53:30 +0900

    replace 'photo' as entity to just a property

--- Domain/ApplicationUser.cs
+++ Domain/ApplicationUser.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 
@@ -9,12 +10,10 @@ namespace Domain
 		public string DisplayName { get; set; }
 		public string Bio { get; set; }
 		public ICollection<PostFollowing> PostFollowings { get; set; }
-		// public Photo Photo { get; set; }
-		public ICollection<Photo> Photos { get; set; }
+		public string ProfilePictureUrl { get; set; }
 		public ApplicationUser()
 		{
 			PostFollowings = new List<PostFollowing>();
-			Photos = new List<Photo>();
 		}
 	}
 }
