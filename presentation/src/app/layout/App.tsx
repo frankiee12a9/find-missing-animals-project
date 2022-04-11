64bf219d35fffd246a07764c5bdf5ba644fb167a
TKNguyen12a9 <cudayanh@gmail.com>
2022-04-11 15:23:50 +0900

    update component routes

--- presentation/src/app/layout/App.tsx
+++ presentation/src/app/layout/App.tsx
@@ -6,11 +6,12 @@ import Leftbar from "./Leftbar"
 import Feed from "./Feed"
 import Rightbar from "./Rightbar"
 import "./styles/App.scss"
-import {Route, Switch} from "react-router"
+import {Route, Switch} from "react-router-dom"
 import { useAppDispatch } from "../store/storeConfig" 
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import HomePage from "./HomePage"
+import MapLandingPage from "../utils/MapLandingPage"
 
 function App() {
 	const dispatch = useAppDispatch()
@@ -39,15 +40,18 @@ function App() {
 		<div>
 			<ThemeProvider theme={theme}>
 				<ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
-        <Navbar darkMode={darkMode} handleThemeChange={handleThemeChange}/>
-        <Route exact path="/" component={HomePage} /> 
-        <Route path={"/(.+)"} render={() => (
-          <Container sx={{mt: 4}}>
-            <Switch>
-              {/* <Route /> */}
-              </Switch>
-          </Container>
-        )} />
+			<Navbar darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+			<Route exact path="/" component={HomePage} /> 
+			<Route path={"/(.+)"} render={() => (
+			<Container sx={{mt: 4}}>
+				<Switch>
+					<Route exact path="/map" component={MapLandingPage}/>
+					{/* <Route exact path="/map" component={MapLandingPage}/>
+					<Route exact path="/map" component={MapLandingPage}/>
+					<Route exact path="/map" component={MapLandingPage}/> */}
+				</Switch>
+			</Container>
+			)} />
 			</ThemeProvider>
 		</div>
 	)
