b41c072590c565b39819d267f5612dc5afa7b727
Kien Nguyen The <cudayanh@gmail.com>
2022-04-16 15:26:50 +0900

    update Tag1Dto

--- UseCases/Posts/Dtos/Tag1Dto.cs
+++ UseCases/Posts/Dtos/Tag1Dto.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace UseCases.Posts.Dtos
 {
 	public class Tag1Dto
 	{
 		public string Tag1Name { get; set; }
+		public ICollection<PostDto> Posts { get; set; }
 	}
 }
