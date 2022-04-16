332fb2ca694591f0349144d756dfcefcbbd90443
Kien Nguyen The <cudayanh@gmail.com>
2022-04-16 15:28:24 +0900

    update Tag1Post

--- Domain/Tag1Post.cs
+++ Domain/Tag1Post.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Domain
 {
-    public class Tag1Post
-    {
-        public  Guid PostId { get; set; }
+	public class Tag1Post
+	{
+		public Guid PostId { get; set; }
 		public Post Post { get; set; }
-		public int Tag1Id { get; set; }
+		public Guid Tag1Id { get; set; }
 		public Tag1 Tag1 { get; set; }
-    }
+	}
 }
--- UseCases/Posts/Dtos/Tag5Dto.cs
+++ UseCases/Posts/Dtos/Tag5Dto.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace UseCases.Posts.Dtos
 {
 	public class Tag5Dto
 	{
 		public string Tag5Name { get; set; }
+		public ICollection<PostDto> Posts { get; set; }
 	}
 }
