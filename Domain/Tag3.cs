6a0f638c3f1672eb5442030687932c7315b56782
Kien Nguyen The <cudayanh@gmail.com>
2022-04-16 15:25:54 +0900

    update Tag3 property

--- Domain/Tag3.cs
+++ Domain/Tag3.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections.Generic;
 
 namespace Domain
 {
 	public class Tag3
 	{
-		public int Id { get; set; }
+		// public int Id { get; set; }
+		public Guid Id { get; set; }
 		public string TagName { get; set; }
 		public ICollection<Tag3Post> Tag3Posts { get; set; }
 		public Tag3()
