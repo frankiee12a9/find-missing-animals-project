1c0587f6cec7cb36a2ad6e5be6b68118b6d73017
Kien Nguyen The <cudayanh@gmail.com>
2022-04-16 15:26:02 +0900

    update Tag4 property

--- Domain/Tag4.cs
+++ Domain/Tag4.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections.Generic;
 
 namespace Domain
 {
 	public class Tag4
 	{
-		public int Id { get; set; }
+		// public int Id { get; set; }
+		public Guid Id { get; set; }
 		public string TagName { get; set; }
 		public ICollection<Tag4Post> Tag4Posts { get; set; }
 		public Tag4()
