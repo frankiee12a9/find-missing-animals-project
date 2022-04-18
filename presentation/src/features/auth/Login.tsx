85ea29b32b2ebb3219c3faad47aa819fc1db4993
Kien Nguyen The <cudayanh@gmail.com>
2022-04-18 14:28:41 +0900

    update Login page

--- presentation/src/features/auth/Login.tsx
+++ presentation/src/features/auth/Login.tsx
@@ -1,7 +1,101 @@
-import React from 'react'
+import {
+	Avatar,
+	Box,
+	Container,
+	Grid,
+	TextField,
+	Typography,
+} from "@material-ui/core"
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+import { Paper } from "@mui/material"
+import { LoadingButton } from "@mui/lab"
+import React from "react"
+import { FieldValues, useForm } from "react-hook-form"
+import { Link, useHistory, useLocation } from "react-router-dom"
+import { useAppDispatch } from "../../app/store/storeConfig"
+import { loginUser } from "./authSlice"
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+	const history = useHistory()
+	const location = useLocation<any>()
+	const dispatch = useAppDispatch()
+	const {
+		register,
+		handleSubmit,
+		formState: { isSubmitting, errors, isValid },
+	} = useForm({
+		mode: "all",
+	})
+
+	async function submitForm(data: FieldValues) {
+		try {
+			await dispatch(loginUser(data))
+			history.push(location.state?.from?.pathname || "/posts")
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	return (
+		<Container
+			component={Paper}
+			maxWidth="sm"
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				p: 4,
+			}}>
+			<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+				<LockOutlinedIcon />
+			</Avatar>
+			<Typography component="h1" variant="h5">
+				Sign in
+			</Typography>
+			<Box
+				component="form"
+				onSubmit={handleSubmit(submitForm)}
+				noValidate
+				sx={{ mt: 1 }}>
+				<TextField
+					margin="normal"
+					fullWidth
+					label="Username"
+					autoFocus
+					{...register("username", {
+						required: "Username is required",
+					})}
+					error={!!errors.username}
+					helperText={errors?.username?.message}
+				/>
+				<TextField
+					margin="normal"
+					fullWidth
+					label="Password"
+					type="password"
+					{...register("password", {
+						required: "Password is required",
+					})}
+					error={!!errors.password}
+					helperText={errors?.password?.message}
+				/>
+				<LoadingButton
+					disabled={!isValid}
+					loading={isSubmitting}
+					type="submit"
+					fullWidth
+					variant="contained"
+					sx={{ mt: 3, mb: 2 }}>
+					Sign In
+				</LoadingButton>
+				<Grid container>
+					<Grid item>
+						{/* Note: redirect to Register */}
+						<Link to="/register">
+							{"Don't have an account? Sign Up"}
+						</Link>
+					</Grid>
+				</Grid>
+			</Box>
+		</Container>
+	)
 }
