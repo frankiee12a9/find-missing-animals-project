73c4680be4ed868784a4388e750340e01b87bfa1
TKNguyen12a9 <cudayanh@gmail.com>
2022-05-13 05:15:06 +0900

    update PagedList

--- UseCases/Core/PagedList.cs
+++ UseCases/Core/PagedList.cs
@@ -10,17 +10,16 @@ namespace UseCases.Core
 	{
 		public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
 		{
-			CurrentPage = pageNumber;
-			TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-			PageSize = pageSize;
-			TotalCount = count;
-			AddRange(items); // handle returning 0 items 
+			Pagination = new Pagination 
+			{
+				CurrentPage = pageNumber,
+				TotalPages = (int)Math.Ceiling(count / (double)pageSize),
+				PageSize = pageSize,
+				TotalCount = count,
+			};
+			AddRange(items);  
 		}
-
-		public int CurrentPage { get; set; }
-		public int TotalPages { get; set; }
-		public int PageSize { get; set; }
-		public int TotalCount { get; set; }
+		public Pagination Pagination { get; set; }
 
 		public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber,
 			int pageSize)
