34b14a01ea72cb104130d883ef12f1492c6436b6
TKNguyen12a9 <cudayanh@gmail.com>
2022-05-13 05:18:19 +0900

    update PostCard

--- presentation/src/features/post/PostCard.tsx
+++ presentation/src/features/post/PostCard.tsx
@@ -3,6 +3,7 @@ import { Favorite, FavoriteBorder, MoreVert, Share } from '@mui/icons-material';
 import {
   Avatar,
   Card,
+  CardActionArea,
   CardActions,
   CardContent,
   CardHeader,
@@ -13,7 +14,7 @@ import {
 } from '@mui/material';
 import { Post } from '../../app/models/post';
 import { useAppSelector } from '../../app/store/storeConfig';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 interface Props {
   img: string;
@@ -22,47 +23,59 @@ interface Props {
 }
 
 export default function PostCard({ img, title, post }: Props) {
+  // const timeNow = Date.now();
+  var today = new Date();
+  var date =
+    today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
   return (
-    <Card sx={{ margin: 5 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVert />
-          </IconButton>
-        }
-        title={post.posterName ? post.posterName : 'Unknown'}
-        subheader={post.createdAt}
-      />
-      <CardMedia
-        component="img"
-        height="20%"
-        image={post.photos.length > 0 ? post.photos[0].url : img}
-        alt="Paella dish"
-      />
-      <CardContent>
-        <CardContent component={NavLink} to={`/posts/${post.id}`}>
-          <Typography gutterBottom variant="h5">
-            {post.title}
-          </Typography>
-          <Typography variant="body2">{post.content}</Typography>
+    <Card sx={{ margin: 2 }}>
+      <CardActionArea
+        LinkComponent={Link}
+        href={`/posts/${post.id}`}
+        // LinkComponent={NavLink}
+        // to={`/posts/${post.id}`}
+      >
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title={post.posterName ? post.posterName : 'Unknown'}
+          // subheader={post.createdAt}
+          subheader={date}
+        />
+        <CardMedia
+          component="img"
+          height="20%"
+          image={post?.photos.length > 0 ? post.photos[0].url : img}
+          alt="Paella dish"
+        />
+        <CardContent>
+          <CardContent component={NavLink} to={`/posts/${post.id}`}>
+            <Typography gutterBottom variant="h5">
+              {post.title}
+            </Typography>
+            <Typography variant="body2">{post.content}</Typography>
+          </CardContent>
         </CardContent>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <Checkbox
-            icon={<FavoriteBorder />}
-            checkedIcon={<Favorite sx={{ color: 'red' }} />}
-          />
-        </IconButton>
-        <IconButton aria-label="share">
-          <Share />
-        </IconButton>
-      </CardActions>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite sx={{ color: 'red' }} />}
+            />
+          </IconButton>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+        </CardActions>
+      </CardActionArea>
     </Card>
   );
 }
