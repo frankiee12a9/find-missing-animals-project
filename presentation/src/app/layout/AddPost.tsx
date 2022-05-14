1a79db6ad96684a4bf1270b2937ce2f238f30267
TKNguyen12a9 <cudayanh@gmail.com>
2022-05-14 18:54:28 +0900

    update AddPost layout

--- presentation/src/app/layout/AddPost.tsx
+++ presentation/src/app/layout/AddPost.tsx
@@ -1,15 +1,43 @@
-import { Add } from '@mui/icons-material';
+import {
+  Add,
+  DateRange,
+  Edit,
+  EmojiEmotions,
+  PersonAdd,
+  VideoCameraBack,
+} from '@mui/icons-material';
 import { SyntheticEvent, useState } from 'react';
 import Postcode from '../utils/Postcode';
 import {
+  Avatar,
+  Box,
   Button,
+  ButtonGroup,
   Container,
+  ExtendModalUnstyled,
   Fab,
   Modal,
+  ModalTypeMap,
   Snackbar,
+  Stack,
+  styled,
   TextField,
   Tooltip,
+  Typography,
 } from '@mui/material';
+
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const UserBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  marginBottom: '20px',
+});
 
 export default function AddPost() {
   const [open, setOpen] = useState(false);
@@ -24,88 +52,71 @@ export default function AddPost() {
   };
   return (
     <>
-      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
-        <Fab color="primary">
-          <Add />
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Delete"
+        sx={{
+          position: 'fixed',
+          bottom: 20,
+          left: { xs: 'calc(50% - 25px)', md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <Edit />
         </Fab>
       </Tooltip>
-      <Modal open={open}>
-        <Container>
-          <form autoComplete="off">
-            <div>
-              <TextField
-                id="standard-basic"
-                label="Title"
-                size="small"
-                style={{ width: '100%' }}
-              />
-            </div>
-            <div>
-              <TextField
-                id="outlined-multiline-static"
-                multiline
-                rows={4}
-                defaultValue="Tell your story..."
-                variant="outlined"
-                label="Description"
-                size="small"
-                style={{ width: '100%' }}
-              />
-            </div>
-            <>
-              <input type="file" id="myFile" name="filename" />
-            </>
-            <br />
-            {/* <div className={classes.item}>
-							<TextField select label="Visibility" value="Public">
-								<MenuItem value="Public">Public</MenuItem>
-								<MenuItem value="Private">Private</MenuItem>
-							</TextField>
-						</div> */}
-            <div>
-              <Postcode />
-              {/* <FormLabel component="legend">
-								Who can comment?
-							</FormLabel>
-							<RadioGroup>
-								<FormControlLabel
-									value="Everybody"
-									control={<Radio size="small" />}
-									label="Everybody"
-								/>
-								<FormControlLabel
-									value="My Friends"
-									control={<Radio size="small" />}
-									label="My Friends"
-								/>
-							</RadioGroup> */}
-            </div>
-            <div>
-              <Button
-                variant="outlined"
-                color="primary"
-                style={{ marginRight: 20 }}
-                onClick={() => setOpenAlert(true)}
-              >
-                등록
-              </Button>
-              <Button
-                variant="outlined"
-                color="secondary"
-                onClick={() => setOpen(false)}
-              >
-                취소
-              </Button>
-            </div>
-          </form>
-        </Container>
-      </Modal>
-      <Snackbar
-        open={openAlert}
-        autoHideDuration={4000}
-        // onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-      ></Snackbar>
+      <StyledModal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          width={537}
+          height={317}
+          bgcolor={'background.default'}
+          color={'text.primary'}
+          p={3}
+          borderRadius={5}
+        >
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} variant="h6">
+              John Doe
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: '100%' }}
+            id="standard-multiline-static"
+            multiline
+            rows={3}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            {/* <Image color="" /> */}
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: '100px' }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
     </>
   );
 }
