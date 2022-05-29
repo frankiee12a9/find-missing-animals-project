f81275d9c97cc774baabae84b3c3b903caf8a4db
TKNguyen12a9 <cudayanh@gmail.com>
2022-05-30 02:06:41 +0900

    update: remove useless code

--- UseCases/Posts/Dtos/PostDto.cs
+++ UseCases/Posts/Dtos/PostDto.cs
@@ -25,13 +25,6 @@ namespace UseCases.Posts
 		[Required]
 		public ICollection<Tag1Dto> Tags { get; set; }
 		public Tag1Dto Tag1 { get; set; }
-
-		// [Required]
-		// public Tag2Dto Tag2 { get; set; }
-		// [Required]
-		// public Tag3Dto Tag3 { get; set; }
-		// public Tag4Dto Tag4 { get; set; }
-		// public Tag5Dto Tag5 { get; set; }
 		public PostLocationDto PostLocation { get; set; }
 		public ICollection<Photo> Photos { get; set; }
 		public DateTime CreatedAt { get; set; }
