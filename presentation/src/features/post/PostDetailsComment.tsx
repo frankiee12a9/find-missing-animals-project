08d0fe17cab7182dd32bca91e597a5fbe2eb5fa5
TKNguyen12a9 <cudayanh@gmail.com>
2022-06-16 00:51:12 +0900

    add: post details comments(SocketIO processing here)

--- presentation/src/features/post/PostDetailsComment.tsx
+++ presentation/src/features/post/PostDetailsComment.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef, useState } from 'react';
 import {
   Paper,
   Grid,
@@ -6,34 +7,144 @@ import {
   TextareaAutosize,
   Button,
 } from '@mui/material';
-import React from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useAppDispatch, useAppSelector } from 'app/store/storeConfig';
+import AppTextInput from 'app/components/AppTextInput';
+import { FieldValues, useForm } from 'react-hook-form';
+import { commentValidationSchema } from 'app/utils/postValidationSchema';
+import LoadingButton from '@mui/lab/LoadingButton/LoadingButton';
+import { PostComment } from 'app/models/comment';
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  LogLevel,
+} from '@microsoft/signalr';
+import moment from 'moment';
 
 interface Props {
   postId: string;
 }
 
-const imgLink =
-  'https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260';
-
 export default function PostDetailsComment({ postId }: Props) {
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.auth);
+  const [comments, setComments] = useState<PostComment[]>([]);
+  const hubConnection = useRef<HubConnection | null>(null);
+
+  useEffect(() => {
+    // enable Socket only for authenticated users
+    if (user?.token) {
+      // connection initialization
+      hubConnection.current = new HubConnectionBuilder()
+        .withUrl(process.env.REACT_APP_CHAT_URL + '?postId=' + postId, {
+          accessTokenFactory: () => user.token,
+        })
+        .withAutomaticReconnect()
+        .configureLogging(LogLevel.Information)
+        .build();
+
+      // start listening to connection
+      hubConnection.current
+        ?.start()
+        .then(() => console.log('hubConnection started'))
+        .catch((err: any) =>
+          console.error('Error establishing hubConnection', err)
+        );
+
+      // load all comments(in current post) from server (1)
+      hubConnection.current?.on('LoadComments', (comments: PostComment[]) => {
+        comments.forEach((comment: PostComment) => {
+          comment.timestamp = new Date(comment.timestamp);
+        });
+
+        console.log('comments loaded', comments);
+        setComments(comments);
+      });
+
+      // receive loaded comments
+      hubConnection.current?.on('ReceiveComment', (comment: PostComment) => {
+        console.log('aComment', comment);
+        comment.timestamp = new Date(comment.timestamp);
+        setComments((currComments) => [comment, ...currComments]);
+      });
+    }
+
+    console.log('_hubConnection', hubConnection.current);
+
+    // unmount the hubConnection
+    return () => {
+      hubConnection.current
+        ?.stop()
+        .then(() => console.log('stopHubConnection'))
+        .catch((err: any) => console.error('Error stopping connection: ', err));
+
+      // temporarily unmount the clear all currently connected comments
+      setComments([]);
+    };
+  }, [postId, dispatch]);
+
+  const {
+    control,
+    reset,
+    handleSubmit,
+    watch,
+    formState: { errors, isDirty, isSubmitting },
+  } = useForm({
+    mode: 'all',
+    resolver: yupResolver<any>(commentValidationSchema),
+  });
+
+  // submit form data
+  const handleSubmitData = async (data: FieldValues) => {
+    const values = { postId: postId, body: data?.body };
+    console.log(values);
+    try {
+      await hubConnection.current
+        ?.invoke('SendComment', values)
+        .then(() => console.log('SendComment success'))
+        .catch((err: any) => console.error(err));
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  // logs form submit errors if occur
+  const submitDataError = (errors: any, event: any) =>
+    console.error('Errors:', errors);
+
+  useEffect(() => {
+    console.log('comments', comments);
+  }, [comments]);
+
   return (
     <>
-      <form>
-        <TextareaAutosize
-          aria-label="Post comments"
-          minRows={5}
-          placeholder="Share your thought here..."
+      <form onSubmit={handleSubmit(handleSubmitData, submitDataError)}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
           style={{
             width: '919px',
             marginLeft: '50px',
             marginBottom: '20px',
             border: '1px solid',
           }}
-        />
+        >
+          <AppTextInput
+            multiline={true}
+            rows={4}
+            control={control}
+            name="body"
+            label=""
+            placeholder="Share your thought here..."
+            resize="none"
+          />
+        </Grid>
         <Button
           variant="contained"
+          type="submit"
           color="primary"
-          style={{ right: '72px', top: '4px' }}
+          style={{ left: '896px', top: '-10px' }}
         >
           Send
         </Button>
@@ -44,47 +155,28 @@ export default function PostDetailsComment({ postId }: Props) {
           width: '919px',
           marginLeft: '50px',
           marginTop: '20px',
+          backgroundColor: '#FFE',
         }}
       >
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item>
-            <Avatar alt="Remy Sharp" src={imgLink} />
-          </Grid>
-          <Grid justifyContent="left" item xs zeroMinWidth>
-            <h4 style={{ margin: 0, textAlign: 'left' }}>Michel Michel</h4>
-            <p style={{ textAlign: 'left' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              luctus ut est sed faucibus. Duis bibendum ac ex vehicula laoreet.
-              Suspendisse congue vulputate lobortis. Pellentesque at interdum
-            </p>
-            <p style={{ textAlign: 'left', color: 'gray' }}>
-              posted 1 minute ago
-            </p>
-          </Grid>
-        </Grid>
-        <Divider variant="fullWidth" style={{ margin: '30px 0' }} />
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item>
-            <Avatar alt="Remy Sharp" src={imgLink} />
-          </Grid>
-          <Grid justifyContent="left" item xs zeroMinWidth>
-            <h4 style={{ margin: 0, textAlign: 'left' }}>Michel Michel</h4>
-            <p style={{ textAlign: 'left' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              luctus ut est sed faucibus. Duis bibendum ac ex vehicula laoreet.
-              Suspendisse congue vulputate lobortis. Pellentesque at interdum
-              tortor. Quisque arcu quam, malesuada vel mauris et, posuere
-              sagittis ipsum. Aliquam ultricies a ligula nec faucibus. In elit
-              metus, efficitur lobortis nisi quis, molestie porttitor metus.
-              Pellentesque et neque risus. Aliquam vulputate, mauris vitae
-              tincidunt interdum, mauris mi vehicula urna, nec feugiat quam
-              lectus vitae ex.{' '}
-            </p>
-            <p style={{ textAlign: 'left', color: 'gray' }}>
-              posted 1 minute ago
-            </p>
-          </Grid>
-        </Grid>
+        {comments.map((aComment) => (
+          <>
+            <Grid container key={aComment.id} wrap="nowrap" spacing={2}>
+              <Grid item>
+                <Avatar alt="Remy Sharp" src={aComment.imageUrl} />
+              </Grid>
+              <Grid justifyContent="left" item xs zeroMinWidth>
+                <h4 style={{ margin: 0, textAlign: 'left' }}>
+                  {aComment.displayName}
+                </h4>
+                <p style={{ textAlign: 'left' }}>{aComment.body}</p>
+                <p style={{ textAlign: 'left', color: 'gray' }}>
+                  {moment(aComment?.timestamp.toISOString()).fromNow()}
+                </p>
+              </Grid>
+            </Grid>
+            <Divider variant="fullWidth" style={{ margin: '30px 0' }} />
+          </>
+        ))}
       </Paper>
     </>
   );
