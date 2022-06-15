39a6c4078bf7c288a7ab76bca290a5287a7dd562
TKNguyen12a9 <cudayanh@gmail.com>
2022-06-16 00:41:32 +0900

    update: '/chat' -> '/comment' for HubConnection

--- API/Startup.cs
+++ API/Startup.cs
@@ -75,7 +75,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHub<CommentHub>("/chat"); 
+                endpoints.MapHub<CommentHub>("/comment"); 
                 endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
