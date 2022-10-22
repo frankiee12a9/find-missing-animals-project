e2e159a7ad3ca96ae1a687bb903b3c55102caf20
TKNguyen12a9 <cudayanh@gmail.com>
2022-10-22 16:37:29 +0900

    feat: add user profile DTO

---
+++ UseCases/ApplicationUsers/Dtos/UserProfileDto.cs
@@ -0,0 +1,10 @@
+namespace UseCases.ApplicationUsers.Dtos
+{
+	public class UserProfileDto
+	{
+		public string Username { get; set; }
+		public string DisplayName { get; set; }
+		public string Bio { get; set; }
+		public string Image { get; set; }
+	}
+}
