f372aaf37b0659574efdcb6b2e3843847fc7e08b
TKNguyen12a9 <cudayanh@gmail.com>
2022-10-22 14:59:55 +0900

    feat: eliminate password complexity

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,7 +9,8 @@ namespace API.Dtos
 		[Required]
 		public string Email { get; set; }
 		[Required]
-		[RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be more complex.")]
+		// [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be more complex.")]
+		[RegularExpression("(?=.*\\d)(?=.*[a-z]).{4,8}$", ErrorMessage = "Password must be more complex.")]
 		public string Password { get; set; }
 		[Required]
 		public string UserName { get; set; }
